d34e047582d2331af43be96d3da88db3498d07be
douglasccaldas <douglasccaldas@gmail.com>
2019-02-16 20:27:55 -0200

    autenticando no servico

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -9,7 +10,7 @@ export class SignInComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
@@ -18,4 +19,18 @@ export class SignInComponent implements OnInit {
     });
   }
 
+  login() {
+    console.log('vai se autenticar');
+    const userName = this.loginForm.get('userName').value;
+    const passWord = this.loginForm.get('passWord').value;
+    this.authService.authenticate(userName, passWord)
+      .subscribe(
+        () => console.log('autenticado'),
+        err => {
+          console.log(err);
+          this.loginForm.reset();
+          alert('Invalid username or password');
+        }
+      );
+  }
 }
