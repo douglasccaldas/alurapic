f1b39cc5f0dd5db45fb2d2db61a7d53c25436610
douglasccaldas <douglasccaldas@gmail.com>
2019-02-20 17:21:14 -0300

    decodificando o token e armazenando como uma interface user

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,7 +10,7 @@ const API_URL = 'http://localhost:3000';
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, private userservice: UserService) { }
 
   authenticate(userName: string, password: string) {
     return this.http
@@ -18,7 +19,7 @@ export class AuthService {
         { observe: 'response' })
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userservice.setToken(authToken);
         window.localStorage.setItem('authToken', authToken);
         console.log(`Usuário ${userName} autenticado com o token ${authToken}`);
       }));
